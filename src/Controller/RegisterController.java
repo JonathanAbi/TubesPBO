20f6d7c24b3e77824984d987a1c439077dc98200
Glenn Pray <glennsagala@gmail.com>
2022-07-06 16:36:03 +0700

    perbaiki error jadi program udh bisa jalan sempurna untuk register dan login, selain itu untuk hashing password udh dipisah jadi mengurangi redundan

--- src/Controller/RegisterController.java
+++ src/Controller/RegisterController.java
@@ -17,19 +17,6 @@ public class RegisterController {
         //for hashing the password
         pass = Hasher.password(pass);
         try {
-//            PreparedStatement stmt = conn.con.prepareStatement(query);
-//            stmt.setString(1, nama);
-//            stmt.setString(2, userName);
-//            stmt.setString(3, pass);
-//            stmt.setString(4, telepon);
-//            stmt.setString(5, alamat);
-//            stmt.setString(6, kelurahan);
-//            stmt.setString(7, kecamatan);
-//            stmt.setString(8, kota);
-//            stmt.setString(9, provinsi);
-//            stmt.setString(10, kodePos);
-//            stmt.executeUpdate();
-
             DatabaseHandler conn = new DatabaseHandler();
             conn.connect();
             PreparedStatement stmt;
@@ -41,9 +28,9 @@ public class RegisterController {
             stmt.setString(3, pass);
             stmt.setString(4, telepon);
             stmt.executeUpdate();
-            
+
             //for insert alamat data
-            query = "INSERT INTO alamat(alamat_status, alamat, kelurahan, kecamatan, kota, provinsi, kodePos) VALUE (0,?,?,?,?,?,?)";
+            query = "INSERT INTO alamat(alamat_status, alamat, kelurahan, kecamatan, kota, provinsi, kode_post) VALUE (0,?,?,?,?,?,?)";
             stmt = conn.con.prepareStatement(query);
             stmt.setString(1, alamat);
             stmt.setString(2, kelurahan);
@@ -52,25 +39,24 @@ public class RegisterController {
             stmt.setString(5, provinsi);
             stmt.setString(6, kodePos);
             stmt.executeUpdate();
-            
+
             Statement stat = conn.con.createStatement();
             ResultSet result;
             //for insert alamat_id to cutomers
-            query = "SELECT alamat_id FROM alamat WHERE alamat = '"+alamat+"' AND kelurahan = '"+kelurahan+"' AND kecamatan = '"+kecamatan+"' AND kota = '"+kota+"' AND provinsi = '"+provinsi+"' AND kode_post = '"+kodePos+"'";
+            query = "SELECT * FROM alamat WHERE alamat = '" + alamat + "' AND kelurahan = '" + kelurahan + "' AND kecamatan = '" + kecamatan + "' AND kota = '" + kota + "' AND provinsi = '" + provinsi + "' AND kode_post = '" + kodePos + "'";
             result = stat.executeQuery(query);
-            int alamatID = result.getInt("alamat_id");
-            
-            query = "UPDATE customers SET alamat_id ="+alamatID+" WHERE username = '"+userName+"'";
+            result.next();
+            int alamatID = result.getInt(1);
+            query = "UPDATE customers SET alamat_id =" + alamatID + " WHERE username = '" + userName + "'";
             stat.executeUpdate(query);
-            
+
             //for insert user_id to alamat
-            query = "SELECT customer_id FROM customers WHERE username = '"+userName+"'";
+            query = "SELECT customer_id FROM customers WHERE username = '" + userName + "'";
             result = stat.executeQuery(query);
-            int customerID = result.getInt("customer_id");
-            
-            query = "UPDATE alamat SET customer_id = '"+customerID+"' alamat = '"+alamat+"' AND kelurahan = '"+kelurahan+"' AND kecamatan = '"+kecamatan+"' AND kota = '"+kota+"' AND provinsi = '"+provinsi+"' AND kode_post = '"+kodePos+"'";
+            result.next();
+            int customerID = result.getInt(1);
+            query = "UPDATE alamat SET customer_id = '" + customerID + "' WHERE alamat = '" + alamat + "' AND kelurahan = '" + kelurahan + "' AND kecamatan = '" + kecamatan + "' AND kota = '" + kota + "' AND provinsi = '" + provinsi + "' AND kode_post = '" + kodePos + "'";
             stat.executeUpdate(query);
-            
             return "Berhasil melakukan registrasi";
 
         } catch (SQLException e) {
