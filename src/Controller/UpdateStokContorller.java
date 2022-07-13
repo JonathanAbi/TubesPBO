45b6d32b594d23dc436a71e01d9cccd08a204216
Glenn Pray <glennsagala@gmail.com>
2022-07-13 14:43:00 +0700

    pebaiki var berat dari String jadi Double

---
+++ src/Controller/UpdateStokContorller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import Database.DatabaseHandler;
+import Model.Produk;
+import java.util.ArrayList;
+/**
+ *
+ * @author glenn
+ */
+public class UpdateStokContorller {
+    DatabaseHandler c = new DatabaseHandler();
+    public ArrayList<Produk> getProdukList() {
+        
+    }
+}
