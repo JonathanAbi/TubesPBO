f7177859e1ec83d15c775a3bf9246ffba66593f2
Glenn Pray <glennsagala@gmail.com>
2022-07-13 15:11:34 +0700

    initialize

---
+++ src/Controller/UpdateStokController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.SingletonProduk;
+/**
+ *
+ * @author glenn
+ */
+public class UpdateStokController {
+    SingletonProduk produk = SingletonProduk.getInstance();
+}
